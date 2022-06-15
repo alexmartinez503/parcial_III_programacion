39042bdd6ea5839b40fd6d4152bb2fc6f923af8e
BryanUGB <bryanpineda2001@gmail.com>
2022-06-14 23:16:25 -0600

    Agregue el estilo y vistas (SMIS941220)

---
+++ zapateria/resources/views/layouts/partials/footer.blade.php
@@ -0,0 +1,7 @@
+<div id="copyrights">
+    <div class="container">
+      <p>
+        &copy; Derechos reservados <strong>Alexis Martinez (SMIS034220), Bryan Pineda (SMIS941220), Andy Sorto(SMIS927320)</strong>
+      </p>
+    </div>
+  </div>
