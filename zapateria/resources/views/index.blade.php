191be47fd36478ec5c51549787578103f3ca0d3f
Andy-lab-oss <as971843@gmail.com>
2022-06-14 23:01:35 -0600

    Agrege Archivos de vistas(SMIS927320)

---
+++ zapateria/resources/views/index.blade.php
@@ -0,0 +1,33 @@
+@extends('layouts.app')
+
+@section('content')
+<div id="headerwrap">
+    <div class="container">
+      <div class="containerbet">
+      <h4>Bienvenido a</h4>
+          <h1>ADOC</h1>
+          <h4>Revisa nuestros estilos</h4>
+      </div>
+    </div>
+    <!-- /container -->
+  </div>
+
+  <section id="works"></section>
+  <div class="container">
+  <center> <h1 class="py-5">Nuestros productos</h1></center>
+    <div class="row centered mt mb">
+
+      <div class="col-lg-4 col-md-4 col-sm-4 gallery">
+        <a><img src="images/guchii.jpg" style="height: 250px; width: 250px;display: block;" class="img-responsive"></a>
+      </div>
+      <div class="col-lg-4 col-md-4 col-sm-4 gallery">
+        <a><img src="images/jordan4.jpg" style="height: 250px; width: 250px;display: block;" class="img-responsive"></a>
+      </div>
+      <div class="col-lg-4 col-md-4 col-sm-4 gallery">
+        <a><img src="images/levis.jpg" style="height: 250px; width: 250px;display: block;" class="img-responsive"></a>
+      </div>
+    </div>
+  </div>
+
+ 
+</div>
